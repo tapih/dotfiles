bbd6af62fde35c754e8ddd1f7e787fe1f19ade93
H.Muraoka <h.muraoka714@gmail.com>
2019-06-08 11:46:30 +0900

    [tmux] fix copy mode

--- install.sh
+++ install.sh
@@ -26,7 +26,7 @@ function make_cache_name() {
     echo ".cache"$1".sh"
 }
 
-sudo apt-get install -y peco htop
+sudo apt-get install -y peco htop xsel
 
 # install tmux plugin manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
