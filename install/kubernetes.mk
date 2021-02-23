18dfa84060870d3dab56308eb4d09afe2ec506f9
Hiroshi Muraoka <h.muraoka714@gmail.com>
2021-02-23 20:05:40 +0900

    [add] Add krew plugin
    Signed-off-by: Hiroshi Muraoka <h.muraoka714@gmail.com>

--- install/kubernetes.mk
+++ install/kubernetes.mk
@@ -4,6 +4,7 @@ KUBERNETES_VERSION := 1.17.15
 KIND_VERSION := 0.10.0
 HELM_VERSION := 3.5.1
 KUSTOMIZE_VERSION := 3.9.4
+KREW_VERSION := 0.4.1
 STERN_VERSION := 1.11.0
 
 HOME_BIN_DIR := $(HOME)/bin
@@ -11,6 +12,7 @@ KUBECTL := $(HOME_BIN_DIR)/kubectl
 KIND := $(HOME_BIN_DIR)/kind
 KUSTOMIZE := $(HOME_BIN_DIR)/kustomize
 HELM := $(HOME_BIN_DIR)/helm
+KREW := $(HOME_BIN_DIR)/krew
 STERN := $(HOME_BIN_DIR)/stern
 
 .PHONY: install
@@ -48,6 +50,16 @@ $(HELM):
 	mkdir -p $(HOME_BIN_DIR)
 	sudo sh -c "$(CURL) https://get.helm.sh/helm-v$(HELM_VERSION)-linux-amd64.tar.gz | tar xz -C $(HOME_BIN_DIR) --strip-components=1"
 	sudo chmod 755 $@
+
+.PHONY: krew
+krew: $(KREW)
+$(KREW):
+	$(CURL) https://github.com/kubernetes-sigs/krew/releases/download/v$(KREW_VERSION)/krew.tar.gz -o /tmp/krew.tar.gz && \
+		cd /tmp && \
+		tar xvzf krew.tar.gz && \
+		mv krew-linux_amd64 $@ && \
+		$(KERW) install krew && \
+		kubectl krew update
 
 .PHONY: stern
 stern: $(STERN)
