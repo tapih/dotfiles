1b4a2bbbf2a016a8bfd60b80dbc248e396e92958
Hiroshi Muraoka <h.muraoka714@gmail.com>
2021-10-27 23:34:07 +0900

    Update misc.sh

--- scripts/misc.sh
+++ scripts/misc.sh
@@ -8,3 +8,5 @@ git clone https://github.com/wbthomason/packer.nvim ~/.config/nvim/packer.nvim
 
 yarn global add bash-language-server0
 yarn global add typescript-language-server
+
+$(brew --prefix)/opt/fzf/install --key-bindings --completion --no-update-rc --no-bash
