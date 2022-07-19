53ec9dbc42b848c34172973d15f4b61dc5bb2a1e
Hiroshi Muraoka <h.muraoka714@gmail.com>
2022-07-20 06:58:36 +0900

    [remove] osx support

--- scripts/xcode.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-sudo xcode-select --switch /Applications/Xcode.app/Contents/Developer
-sudo xcodebuild -runFirstLaunch
-sudo xcodebuild -license
-arch -x86_64 sudo gem install ffi
-sudo gem install cocoapods
-arch -x86_64 open -a /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app/
-
