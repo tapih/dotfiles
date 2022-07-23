586201f2859aa60763a0bd9b679c0eb0f65c42ca
Hiroshi Muraoka <h.muraoka714@gmail.com>
2022-07-23 21:18:57 +0900

    [update] clone depth 1

--- scripts/misc.sh
+++ scripts/misc.sh
@@ -1,16 +1,18 @@
 #! /bin/sh
 
-mkdir -p ~/.tmux/plugins/tpm
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+mkdir -p ~/.tmux/plugins
+git clone --depth 1 https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 mkdir -p ~/.zsh
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 
 mkdir -p ~/.config/nvim
-git clone https://github.com/wbthomason/packer.nvim ~/.config/nvim/packer.nvim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.config/nvim/packer.nvim
 
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.2
+mkdir -p ~/.asdf
+git clone --depth 1 https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.2
 
+mkdir -p ~/.fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 
 mkdir -p ~/.zsh/docker
