c4c25941115bcf028b97bb24f77480054ecdb09b
Hiroshi Muraoka <h.muraoka714@gmail.com>
2023-02-12 11:46:14 +0900

    [add] lazygit config

--- scripts/dot.sh
+++ scripts/dot.sh
@@ -5,18 +5,18 @@ set -o pipefail
 
 if [ $# -lt 2 ]
 then
-  echo "USAGE: dot.sh <target> <destination>" 1>&2
+  echo "USAGE: dot.sh <input> <source dir>" 1>&2
   exit 1
 fi
 
-target=$1
-source_dir=$2
+input=$1
+src=$2
 
-for i in $(grep -vE "^\s*#" ${target})
+for i in $(grep -vE "^\s*#" ${input})
 do
   echo ${i}
   if [ -L ${HOME}/${i} ]; then continue; fi
   if [ -f ${HOME}/${i} ]; then rm -f ${HOME}/${i}; fi
   mkdir -p ${HOME}/$(dirname ${i})
-  ln -s ${source_dir}/${i} ${HOME}/${i}
+  ln -s ${src}/${i} ${HOME}/${i}
 done
