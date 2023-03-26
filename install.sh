4ee729af71b2ec3156fe47c8f54f74f95ed80d09
Hiroshi Muraoka <h.muraoka714@gmail.com>
2023-03-26 13:09:16 +0000

    [add] Ghfile

--- install.sh
+++ install.sh
@@ -10,3 +10,4 @@ echo "=== brew ==="; ./scripts/brew.sh Brewfile
 echo "=== go ==="  ; ./scripts/go.sh   Gofile # depends on Brewfile
 echo "=== npm ===" ; ./scripts/npm.sh  Npmfile # depends on Brewfile
 echo "=== krew ==="; ./scripts/krew.sh Krewfile # depends on Brewfile
+echo "=== gh ===";   ./scripts/gh.sh   Ghfile  # depends on Brewfile
