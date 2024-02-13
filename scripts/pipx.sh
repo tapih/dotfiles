8987182be06e8e61e2791f8e3f4e638d152f5734
Hiroshi Muraoka <h.muraoka714@gmail.com>
2024-02-13 10:21:10 +0900

    [add] Pipxfile

---
+++ scripts/pipx.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+set -eu
+set -o pipefail
+
+if [ $# -lt 1 ]
+then
+  echo "USAGE: pipx.sh <file>" 1>&2
+  exit 1
+fi
+
+file=$1
+
+for i in $(grep -vE "^\s*#" ${file} | tr "\n" " ")
+do
+  echo ${i}
+  pipx install ${i} || exit 1
+done
+
