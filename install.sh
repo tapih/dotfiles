e42904a81921969d032593ff997f53f019e4d897
Hiroshi Muraoka <h.muraoka714@gmail.com>
2024-11-06 23:45:04 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,5 @@ echo "=== dot ===" ; ./scripts/dot.sh  Dotfile $(pwd)/dotfiles
 echo "=== brew ==="; ./scripts/brew.sh Brewfile
 echo "=== go ==="  ; ./scripts/go.sh   Gofile # depends on Brewfile
 echo "=== go ==="  ; ./scripts/pipx.sh Pipxfile # depends on Brewfile 
-echo "=== npm ===" ; ./scripts/npm.sh  Npmfile # depends on Brewfile
 echo "=== krew ==="; ./scripts/krew.sh Krewfile # depends on Brewfile
 echo "=== gh ===";   ./scripts/gh.sh   Ghfile  # depends on Brewfile
