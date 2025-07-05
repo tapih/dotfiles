c9bf47d7c74ef1ac1cd8053bfd73e1af33425f05
Hiroshi Muraoka <h.muraoka714@gmail.com>
2025-07-05 19:28:11 +0900

    feat: update keys

--- dotfiles/.config/nvim/lua/plugins/reviewthem.lua
+++ dotfiles/.config/nvim/lua/plugins/reviewthem.lua
@@ -1,18 +1,17 @@
 return {
   "KEY60228/reviewthem.nvim",
-  lazy = true,
   dependencies = {
     "sindrets/diffview.nvim",
     "nvim-telescope/telescope.nvim",
   },
   keys = {
-    { "<leader>rr", "<cmd>ReviewThemStart<cr>", desc = "Start Review" },
-    { "<leader>rc", "<cmd>ReviewThemAddComment<cr>", desc = "Add Comment" },
-    { "<leader>rk", "<cmd>ReviewThemSubmit<cr>", desc = "Submit Review" },
-    { "<leader>ra", "<cmd>ReviewThemAbort<cr>", desc = "Abort Review" },
-    { "<leader>rl", "<cmd>ReviewThemShowComments<cr>", desc = "Show Comments" },
-    { "<leader>rt", "<cmd>ReviewThemToggleReviewed<cr>", desc = "Toggle Reviewed" },
-    { "<leader>rs", "<cmd>ReviewThemStatus<cr>", desc = "Show Status" },
+    { "<leader>ab", "<cmd>ReviewThemStart<cr>", desc = "Start Review" },
+    { "<leader>am", "<cmd>ReviewThemAddComment<cr>", desc = "Add Comment" },
+    { "<leader>ao", "<cmd>ReviewThemSubmit<cr>", desc = "Submit Review" },
+    { "<leader>ax", "<cmd>ReviewThemAbort<cr>", desc = "Abort Review" },
+    { "<leader>al", "<cmd>ReviewThemShowComments<cr>", desc = "Show Comments" },
+    { "<leader>at", "<cmd>ReviewThemToggleReviewed<cr>", desc = "Toggle Reviewed" },
+    { "<leader>as", "<cmd>ReviewThemStatus<cr>", desc = "Show Status" },
   },
   config = function()
     require("reviewthem").setup({})
