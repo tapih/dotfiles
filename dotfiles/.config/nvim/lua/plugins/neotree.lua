895fd7ecb4aef43b7c6adf600bedc6c57f64f055
Hiroshi Muraoka <h.muraoka714@gmail.com>
2025-07-06 11:23:57 +0900

    feat: show hidden

--- dotfiles/.config/nvim/lua/plugins/neotree.lua
+++ dotfiles/.config/nvim/lua/plugins/neotree.lua
@@ -7,5 +7,17 @@ return {
       width = 40,
       height = 30,
     },
+    filesystem = {
+      filtered_items = {
+        hide_dotfiles = false,
+        hide_gitignored = false,
+        hide_hidden = false,
+        hide_by_name = {
+          ".git",
+          "node_modules",
+          "vendor",
+        },
+      },
+    },
   },
 }
