2bef01d78d0ade2ffc044243f08b0a20459d1e35
Hiroshi Muraoka <h.muraoka714@gmail.com>
2025-07-06 12:33:48 +0900

    feat: TODO list

--- dotfiles/.config/nvim/lua/plugins/snacks.lua
+++ dotfiles/.config/nvim/lua/plugins/snacks.lua
@@ -2,5 +2,26 @@ return {
   "snacks.nvim",
   opts = {
     lazygit = { enabled = true },
+    snacks = {
+      enabled = true,
+      default = "scratch",
+      scratch = {
+        filetype = "markdown",
+        name = "Scratch Buffer",
+        autocmds = {
+          { "BufEnter", "*", "setlocal nonumber norelativenumber" },
+          { "FileType", "markdown", "setlocal wrap" },
+        },
+      },
+    },
+  },
+  keys = {
+    {
+      "<leader>T",
+      function()
+        Snacks.scratch({ name = "TODO", ft = "markdown" })
+      end,
+      desc = "Open TODO Scratch Buffer",
+    },
   },
 }
