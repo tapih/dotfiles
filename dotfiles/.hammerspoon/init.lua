94e374bd6887494386190d1aed25f7a09ff1d6fe
Hiroshi Muraoka <h.muraoka714@gmail.com>
2025-07-11 18:01:48 +0900

    feat: hammerspoon
    Signed-off-by: Hiroshi Muraoka <h.muraoka714@gmail.com>

---
+++ dotfiles/.hammerspoon/init.lua
@@ -0,0 +1,22 @@
+local function launchOrRaise(appName, appPath)
+	local app = hs.application.find(appName)
+	if app then
+		app:activate()
+	else
+		hs.application.launchOrFocus(appPath or appName)
+	end
+end
+
+local hyper = { "ctrl", "alt" }
+
+hs.hotkey.bind(hyper, "1", function()
+	launchOrRaise("Slack", "/Applications/Slack.app")
+end)
+
+hs.hotkey.bind(hyper, "2", function()
+	launchOrRaise("Google Chrome", "/Applications/Google Chrome.app")
+end)
+
+hs.hotkey.bind(hyper, "3", function()
+	launchOrRaise("Alacritty", "/Applications/Alacritty.app")
+end)
