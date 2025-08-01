5ee797c5c0d89c57f1dc4c5a551b06c8a88ac41a
Hiroshi Muraoka <h.muraoka714@gmail.com>
2025-08-02 01:35:37 +0900

    feat: install neotest

--- dotfiles/.config/nvim/lua/config/lazy.lua
+++ dotfiles/.config/nvim/lua/config/lazy.lua
@@ -25,6 +25,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.editor.inc-rename" },
     { import = "lazyvim.plugins.extras.editor.neo-tree" },
     { import = "lazyvim.plugins.extras.editor.overseer" },
+    { import = "lazyvim.plugins.extras.editor.aerial" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.git" },
