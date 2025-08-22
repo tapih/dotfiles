86a54769507d5fe7a353214fb6b57c52a2074d67
Hiroshi Muraoka <h.muraoka714@gmail.com>
2025-08-22 20:34:55 +0900

    feat: update cfp.nvim

--- dotfiles/.config/nvim/lua/plugins/cfp.lua
+++ dotfiles/.config/nvim/lua/plugins/cfp.lua
@@ -1,8 +1,5 @@
 return {
   "tapihdev/cfp.nvim",
-  keys = {
-    { "<leader>cp", "<cmd>CopyPath<cr>", desc = "Copy file path" },
-    { "<leader>cP", "<cmd>CopyPathLine<cr>", desc = "Copy file path with line number" },
-    { "<leader>cw", "<cmd>CopyPathURL<cr>", desc = "Copy file path as GitHub URL" },
-  },
+  event = "VeryLazy",
+  config = true
 }
